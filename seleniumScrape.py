cfc80581d2b860fe2e696af83c52d8d09acf5819
Sakshamsetia <setiasaksham15@gmail.com>
2025-09-06 19:40:02 +0530

    Browser error  of selenium scrapper solved

--- seleniumScrape.py
+++ seleniumScrape.py
@@ -1,8 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def seleniumScraper(url):
-    browser = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless")        # run in background
+    options.add_argument("--disable-gpu")     # recommended
+    options.add_argument("--no-sandbox")      # for Linux
+    options.add_argument("--disable-dev-shm-usage")
+    browser = webdriver.Chrome(options=options)
+
     browser.get(url)
-    html = browser.execute_script("return document.documentElement.outerHTML;")
+    html = browser.page_source
     browser.close()
     return html
